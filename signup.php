961f919245779c49a5afd78bcd6f4b112eda00b6
psyfer <psyferblock.io@gmail.com>
2022-05-29 00:08:27 +0300

    add condition for sign up to check for email

--- signup.php
+++ signup.php
@@ -12,15 +12,29 @@ $dob=$_GET("dob");
 $password=$_GET("password");
 $user_type=$_GET("user_type");
 
-// the query 
+// the condidtion for sign up
 
-$query=$mysqli->prepare("INSERT into users(full_name,email,gender,date_of_birth,password,user_type) values (?,?,?,?,?,?)");
-$query->bind_param("ssissi", $full_name, $email, $gender, $dob,$password,$user_type);
-$querry-> execute();
+$query = $mysqli->prepare("SELECT id from users where email = ? AND password = ?");
+$query->bind_param("ss", $email, $password);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows;
+$query->bind_result($id);
+$query->fetch();
+$response = [];
 
-$response=[];
-$response["success"]=true;
-echo json_encode($response);
+if ($num_rows == 1) {
+    $response["response"] = "account already used";
+} else {
+    query=$mysqli->prepare("INSERT into users(full_name,email,gender,date_of_birth,password,user_type) values (?,?,?,?,?,?)");
+    $query->bind_param("ssissi", $full_name, $email, $gender, $dob,$password,$user_type);
+    $querry-> execute();
+    $response=[];
+    $response["success"]=true;
+
+}
+
+// the query once the condition is met 
 
 
 
