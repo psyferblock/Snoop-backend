d97f9c3bd8f4f267a960cb4b8d44296cb3c068dd
psyfer <psyferblock.io@gmail.com>
2022-05-30 20:06:29 +0300

    reviews api

--- addreview.php
+++ addreview.php
@@ -1,2 +1,22 @@
-// add a review to a certain restaurant
-// receive: review text, rating score, status by default 0, user id, resto id,
+
+
+<?php
+ include("connection.php");
+
+// api to add the restaurant features by admin 
+
+ $id=$_GET["restaurant_id"];
+ $review=$_GET["review_text"];
+ $rating=$_GET["rating_score"];
+
+//  the query !
+
+ $query= $mysqli->prepare("INSERT into reviews (review_text,rating_score) values( ?,?) where restaurants_restaurant_id=?");
+ $query->bind_param("sss", $id, $review, $rating);
+ $query->execute();
+ $response = [];
+ $response["success"] = true;
+
+ echo json_encode($response);
+
+?>
