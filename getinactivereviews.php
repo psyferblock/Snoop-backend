887bd5f7a14d34406c3f9796e0ee5e8c4911ea26
psyfer <psyferblock.io@gmail.com>
2022-05-30 21:44:59 +0300

    fixing getrestaurant reviews api

--- getinactivereviews.php
+++ getinactivereviews.php
@@ -2,15 +2,15 @@
 
 include("connection.php");
 
-$id=$_GET["id"];
+// $id=$_GET["id"];
 
 // the query that will call the reviews and ratings 
 
-$query=$mysqli->query("SELECT review_text,rating_score from reviews where status='0' and restaurants_restaurant_id = ? ");
+$query=$mysqli->prepare("SELECT * from reviews where status='0' ");
 
 // binding the query executing and calling the json function
 
-$query->bind_param("i", $id);
+// $query->bind_param("i", $id);
 $query->execute();
 $array=$query->get_result();
 $response[]=$array->fetch_assoc();
