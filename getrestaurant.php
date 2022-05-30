d491b16cac1e7a6c62465beedf7b93e11a020e11
psyfer <psyferblock.io@gmail.com>
2022-05-30 12:48:29 +0300

    apis revisited

--- getrestaurant.php
+++ getrestaurant.php
@@ -6,8 +6,8 @@ include("connection.php");
 $id = $_GET["id"];
 
 $query = $mysqli->prepare("select rest_name,address,description,cover_image_url from restaurants where id=?");
-$query ->bind_param("i",$id);
+// $query ->bind_param("i",$id);
 $query->execute();
 $json=json_encode($query);
-echo " $json";
+echo $json;
 ?>
