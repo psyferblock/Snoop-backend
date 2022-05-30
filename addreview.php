d9b2123db5347f9496674186fa1071156ca95747
psyfer <psyferblock.io@gmail.com>
2022-05-30 21:12:41 +0300

    sql for add review file

--- addreview.php
+++ addreview.php
@@ -5,15 +5,15 @@
 
 // api to add the restaurant features by admin 
 
- $id=$_GET["restaurant_id"];
- $user_id=$_GET["user_id"];
- $review=$_GET["review_text"];
- $rating=$_GET["rating_score"];
+ $id=$_POST["restaurant_id"];
+ $user_id=$_POST["user_id"];
+ $review=$_POST["review_text"];
+ $rating=$_POST["rating_score"];
 
 //  the query !
 
- $query= $mysqli->prepare("INSERT into reviews (review_text,rating_score) values( ?,?) where restaurants_restaurant_id=$id and users_user_id=$user_id");
- $query->bind_param("ss",  $review, $rating);
+ $query= $mysqli->prepare("INSERT into reviews (review_text,rating_score,restaurants_restaurant_id,users_user_id) values( ?,?,?,?) ");
+ $query->bind_param("ssii",  $review, $rating,$id,$user_id);
  $query->execute();
  $response = [];
  $response["success"] = true;
