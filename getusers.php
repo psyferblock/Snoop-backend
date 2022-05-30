193ab0b7b1b6191eaa187c7879a6c89e02d85848
Hsein Bitar <hseinbitar22@gmail.com>
2022-05-30 18:23:09 +0300

    pushing so we can branch out

--- getusers.php
+++ getusers.php
@@ -1,13 +1,12 @@
-<!-- // get all users with their relevant information
-
-
-
-// charbel example -->
 
 <?php
-
-
 include("connection.php");
+
+$user_id = $_POST["user_id"];
+
+// get user id from post
+// return users if user id is admin
+
 
 
 $query = $mysqli->prepare("SELECT * from users");
