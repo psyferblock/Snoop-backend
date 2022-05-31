a01550882315a02d6822b73a6575b495de1fb872
Hsein Bitar <hseinbitar22@gmail.com>
2022-05-31 15:15:26 +0300

    fixed semicolons on end of lines

--- upgradeuser.php
+++ upgradeuser.php
@@ -7,13 +7,13 @@ $target_id=$_POST["target_id"];
 
 // the variables
 
-$user_type=$__POST["user_type"]
+$user_type=$__POST["user_type"];
 $password=hash("sha256", $_POST["password"]);
 $email = $_POST["email"];
 $full_name = $_POST["full_name"];
 
 // the query 
-$user_query = $mysqli->prepare("SELECT user_type from users where user_id=?")
+$user_query = $mysqli->prepare("SELECT user_type from users where user_id=?");
 $user_query->bind_param("i",$user_id);
 $user_query->execute();
 $response=[];
