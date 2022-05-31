c6260356fe173f6da7c95284272a083040a32ba5
Hsein Bitar <hseinbitar22@gmail.com>
2022-05-31 23:01:27 +0300

    finalized APIs, still need some security features

--- addrestaurant.php
+++ addrestaurant.php
@@ -2,18 +2,34 @@
 include("connection.php");
 
 // api to add the restaurant features by admin 
- $name=$_POST["restaurant_name"];
- $address=$_POST["address"];
- $description=$_POST["description"];
- $cover_image=$_POST["cover_image"];
+$name = $_POST["restaurant_name"];
+$address = $_POST["address"];
+$description = $_POST["description"];
+
+$random_string = generateRandomString();
+$cover_uri_for_save = $_SERVER['DOCUMENT_ROOT'] . '/snoop/Snoop-backend/restimages/' . $random_string . '.png';
+$cover_uri_for_uri = '/Snoop-backend/restimages/' . $random_string . '.png';
+
+echo $cover_uri_for_save;
+
+// get image base64 data and save it in restimages
+$imgData = str_replace(' ', '+', $_POST["cover_image"]);
+$imgData =  substr($imgData, strpos($imgData, ",") + 1);
+$imgData = base64_decode($imgData);
+// Path where the image is going to be saved
+$filePath = $cover_uri_for_save;
+// Write $imgData into the image file
+$file = fopen($filePath, 'w');
+fwrite($file, $imgData);
+fclose($file);
+
 
 //  the query !
 
- $query= $mysqli->prepare("INSERT into restaurants (rest_name,address,description,cover_image_uri) values( ?,?,?,?)");
- $query->bind_param("ssss", $name, $address, $description, $cover_image);
- $query->execute();
- $response = [];
- $response["success"] = true;
+$query = $mysqli->prepare("INSERT into restaurants (rest_name,address,description,cover_image_uri) values( ?,?,?,?)");
+$query->bind_param("ssss", $name, $address, $description, $cover_uri_for_uri);
+$query->execute();
+$response = [];
+$response["success"] = true;
 
- echo json_encode($response);
-?>
+echo json_encode($response);
--- connection.php
+++ connection.php
@@ -8,4 +8,13 @@ $db_pass = null;
 $db_name = "snoopdb";
 
 $mysqli = new mysqli($host, $db_user, $db_pass, $db_name);
-?>
+
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
